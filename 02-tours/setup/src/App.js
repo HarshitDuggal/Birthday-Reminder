fc885c1dea8cea92738e0c67b2921cc2fb0edf34
Harshit Duggal <harshitduggal101@gmail.com>
2021-07-19 03:20:35 +0530

    display tours

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -4,8 +4,38 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
-function App() {
-  return <h2>Tours Project Setup</h2>
+const App = () => {
+const [loading, setloading] = useState(true);
+const [tours, settours] = useState([]);
+
+const fetchTours = async () => {
+  try {
+    const response =await fetch(url);
+    const tours = await response.json();
+    setloading(false)
+    settours(tours)
+  } catch (error) {
+    setloading(false)
+    console.log(error);
+  }
+    }
+useEffect(() => {
+  fetchTours();
+}, []);
+
+if (loading) {
+  return (
+      <main>
+        <Loading/>
+      </main>
+    );
+  }
+
+  return <>
+    <main>
+      <Tours tours={tours}/>
+    </main> 
+  </>
 }
 
 export default App
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id,image,price,info,name}) => {
+  return <article className='single-tour'>
+    <img src={image} alt={name}/>
+      <footer className='tour-info'>
+        <h4>{name}</h4>
+        <h4 className='tour-price'>${price}</h4>
+      </footer>      
+      <p>{info}</p>
+      <button className='delete-btn'>Not interested</button>
+  </article>
 };
 
 export default Tour;
